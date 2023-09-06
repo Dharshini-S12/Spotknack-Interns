e252ad643c1a258c1d597b3a2e7f82af620d8872
Dharshini-S12 <118833017+Dharshini-S12@users.noreply.github.com>
2023-09-06 19:58:30 +0530

    commit

---
+++ assets/script.js
@@ -0,0 +1,4 @@
+const navigateButton = document.getElementById('navigateButton');
+navigateButton.addEventListener('click', function () {
+    window.location.href = 'http://www.linkedin.com/in/dharshini-selvaraj';
+});
